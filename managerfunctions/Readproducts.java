75953f4008a30139e14100d9bc2b63f30743e105
AKUL SHARMA <akulsharma02@gmail.com>
2022-01-13 12:31:09 -0800

    Added read products

---
+++ managerfunctions/Readproducts.java
@@ -0,0 +1,24 @@
+package managerfunctions;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.google.gson.*;
+public class Readproducts {
+    public static inventory.ProductList ReadProducts() {
+        Gson gson = new Gson();
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get("./inventory/inventory.json"));
+            inventory.ProductList productlist = gson.fromJson(reader,inventory.ProductList.class);
+            return productlist;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Readproducts.ReadProducts();
+    }
+}
